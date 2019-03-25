4ad80c8c928ad66643733c8818d1c217fffb1d8a
Claudia Luna Vargas <lunezz@gmail.com>
2019-03-25 13:57:49 -0600

    initial commit

---
+++ src/api/config.js
@@ -0,0 +1,11 @@
+export default{
+    appname: 'app-music',
+    apiKey: '7055104e1c2d5e65cb9f1cf709c5fd16',
+    secret: '755b3d043609ec8cf573a9a95ead2fd5',
+    registeredTo: 'app-music'
+}
+
+// Application name	app-music
+// API key	7055104e1c2d5e65cb9f1cf709c5fd16
+// Shared secret	755b3d043609ec8cf573a9a95ead2fd5
+// Registered to	app-music
